5589da74a7990ad2becf7505c3b9fb55ae55334f
ArTim <3a932118@gm.student.ncut.edu.tw>
2022-11-16 01:03:50 +0800

    練習2

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +21,13 @@ Route::get('/', function () {
     //return view('post');
     //return view('contact');
     //return view('about');
-    return redirect(route('posts.index'));
+    // return redirect(route('posts.index'));
+    $post = new Post();
+    $post->title = 'test title';
+    $post->content = 'test content';
+    $post->save();
+    return 'Saved, OK!';
+
 });
 
 Route::get('posts', [PostController::class, 'index'])->name('posts.index');
