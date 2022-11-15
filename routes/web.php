48ef9b14873b16d7503df10d778ec045824ecb28
ArTim <3a932118@gm.student.ncut.edu.tw>
2022-11-16 01:08:27 +0800

    練習4-2

--- routes/web.php
+++ routes/web.php
@@ -34,10 +34,16 @@ Route::get('/', function () {
     // dd($posts);
     // $post = Post::find(1);
     // dd($post);
-    $posts = Post::where('id', '<', 10)->orderBy('id', 'DESC')->get();
-    dd($posts);
+    // $posts = Post::where('id', '<', 10)->orderBy('id', 'DESC')->get();
+    // dd($posts);
 
-    return 'Saved, OK!';
+    // return 'Saved, OK!';
+    $post = Post::find(1);
+    $post->update([
+        'title' => 'updated title',
+        'content' => 'updated content',
+    ]);
+    return 'Updated, OK!';
 
 });
 
